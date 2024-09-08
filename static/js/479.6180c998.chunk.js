fb90bc2fb9b380c45eebd045be5f36ec13e9ec68
ChingizAga <155936343+ChingizAga@users.noreply.github.com>
2024-09-07 21:49:39 -0400

    Add files via upload

---
+++ static/js/479.6180c998.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfinal_project=self.webpackChunkfinal_project||[]).push([[479],{479:(e,n,c)=>{c.r(n),c.d(n,{default:()=>s});var r=c(579);function s(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsxs)("section",{className:"container",children:[(0,r.jsx)("h1",{children:"404"}),(0,r.jsx)("p",{children:"Not Found"})]})})}}}]);
+//# sourceMappingURL=479.6180c998.chunk.js.map
